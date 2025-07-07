3a5021e8098281a6a9c4388c2686994aac75031c
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-07-07 13:41:31 +0200

    Added code to display a square on the screen. Also added code to get keyboard input and track window resizing

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,38 @@
+#include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/WindowStyle.hpp>
+#include <cstdio>
 #include <iostream>
-#include <SFML/Graphics.hpp>
 
-int main(){
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Pong Clone");
+int main() {
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Pong Clone",
+                          sf::Style::Default);
+  sf::RectangleShape player(sf::Vector2f(100, 100));
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
+      switch (event.type) {
+      case sf::Event::Closed:
         window.close();
+        break;
+      case sf::Event::Resized:
+        printf("New Window Width: %i\nNew Window Height: %i\n",
+               event.size.width, event.size.height);
+        break;
+      case sf::Event::TextEntered:
+        if (event.text.unicode < 128) {
+          printf("%c\n", event.text.unicode);
+        }
+        break;
+      }
     }
-    window.clear(sf::Color::Black);
+    window.draw(player);
     window.display();
   }
- return 0; 
+  return 0;
 }

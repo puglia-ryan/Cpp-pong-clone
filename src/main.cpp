dd050b452531f642545dc6a30bfd46f806a98cbf
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-07-07 12:31:36 +0200

    Added code to display a basic black window using SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main(){
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Pong Clone");
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+    window.clear(sf::Color::Black);
+    window.display();
+  }
  return 0; 
 }

1e3f0de62300c2379d043c1bcc7613ffe2c02071
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-07-09 01:28:41 +0200

    Implemented remaining methods for Ball class

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -1,5 +1,9 @@
 #include "Ball.h"
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <cstdlib>
 
 Ball::Ball(float radius)
   : mVelocity(0.f, 0.f)
@@ -9,3 +13,38 @@ Ball::Ball(float radius)
   mShape.setFillColor(sf::Color::White);
 }
 
+void Ball::update(float dt) {
+  mShape.move(mVelocity * dt);
+}
+
+void Ball::draw(sf::RenderWindow& window){
+  window.draw(mShape);
+}
+
+void Ball::bounceX(){
+  mVelocity.x = -mVelocity.x;
+}
+
+void Ball::bounceY(){
+  mVelocity.y = -mVelocity.y;
+}
+
+void Ball::setPosition(float x, float y){
+  mShape.setPosition(x, y);
+}
+
+void Ball::setVelocity(const sf::Vector2f& velocity){
+  mVelocity = velocity;
+}
+
+float Ball::getRadius() const {
+  return mShape.getRadius();
+}
+
+sf::Vector2f Ball::getPosition() const {
+  return mShape.getPosition();
+}
+
+sf::FloatRect Ball::getGlobalBounds() const {
+  return mShape.getGlobalBounds();
+}

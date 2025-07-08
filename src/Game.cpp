3a12f5adf099fe60477d519fc50ece6d2439b121
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-07-09 01:38:26 +0200

    Implemented constructor for Game class

---
+++ src/Game.cpp
@@ -0,0 +1,25 @@
+#include "Game.h"
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/WindowStyle.hpp>
+#include <stdexcept>
+
+Game::Game()
+  : mWindow({WindowWidth, WindowHeight}, "Pong Clone", sf::Style::Close),
+  mPlayer1(sf::Vector2f(10.f, WindowHeight / 2.f), sf::Vector2f(20.f, 100.f), sf::Keyboard::W, sf::Keyboard::S),
+  mPlayer2(sf::Vector2f(WindowWidth - 10.f, WindowHeight / 2.f), sf::Vector2f(20.f, 100.f), sf::Keyboard::Up, sf::Keyboard::Down),
+  mBall(10.f) {
+    if (!mFont.loadFromFile("assets/arial.ttf")) {
+      throw std::runtime_error("Failed to load font");
+    }
+
+    // Set up score texts
+    mScoreText1.setFont(mFont);
+    mScoreText1.setCharacterSize(30);
+    mScoreText1.setPosition(WindowWidth * 0.25f, 10.f);
+
+    mScoreText2 = mScoreText1;
+    mScoreText2.setPosition(WindowWidth * 0.75f, 10.f);
+
+    resetBall();
+  }

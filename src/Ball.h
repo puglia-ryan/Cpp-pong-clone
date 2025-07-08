53f5640b9cdc0d642e1a4bf0bda72af0464d0eb0
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-07-09 00:44:33 +0200

    Altered interface of Ball class

--- src/Ball.h
+++ src/Ball.h
@@ -1,4 +1,5 @@
 #include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
 class Ball{
@@ -6,8 +7,17 @@ class Ball{
     Ball(float radius);
     void update(float dt);
     void draw(sf::RenderWindow&);
-    void reset();
+    void bounceX();
+    void bounceY();
+
+    void setPosition(float x,float y);
+    void setVelocity(const sf::Vector2f& velocity);
+
+    float getRadius() const;
+    sf::Vector2f setPosition() const;
+    sf::FloatRect getGlobalBounds() const;
+
   private:
-    sf::CircleShape shape;
-    sf::Vector2f velocity;
+    sf::CircleShape mShape;
+    sf::Vector2f mVelocity;
 };

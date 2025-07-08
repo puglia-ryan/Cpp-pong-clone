677c85961f22d03886c4629703e0416eae1cd849
Ryan Puglia <ryan.puglia.001@student.uni.lu>
2025-07-08 20:17:55 +0200

    Updated the interface for the Paddle class

--- src/Paddle.h
+++ src/Paddle.h
@@ -1,12 +1,18 @@
+#include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Keyboard.hpp>
 class Paddle{
   public:
-    Paddle(float x, float y);
-    void handleInput(float dt);
+    Paddle(const sf::Vector2f& position, const sf::Vector2f& size, sf::Keyboard::Key upKey, sf::Keyboard::Key downKey);
     void update(float dt);
     void draw(sf::RenderWindow&);
+    sf::FloatRect getGlobalBounds() const;
+    sf::Vector2f getPosition();
   private:
     sf::RectangleShape shape;
     float speed = 400.f;
+    sf::Keyboard::Key mUpKey;
+    sf::Keyboard::Key mDownKey;
 };
